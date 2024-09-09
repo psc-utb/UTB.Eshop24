7ac53dd8b01649a0de39d7df23cece5a50ccf01c
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-09-09 15:44:28 +0200

    Vytvoření DbContextu pro databázi
    + Vytvoření EshopDbContext s DbSety Products a Carousels
    ~ Přidání connection stringu pro připojení aplikace na MySQL databázi (do appsettings.json)
    ~ Přidání konfigurace Entity Framework Core a EshopDbContextu s aplikováním connection stringu (v Program.cs)

---
+++ UTB.Eshop.Infrastructure/Database/EshopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Infrastructure.Database.Seeding;
+
+namespace UTB.Eshop.Infrastructure.Database
+{
+    public class EshopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Carousel> Carousels { get; set; }
+
+        public EshopDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+}
--- UTB.Eshop24/Program.cs
+++ UTB.Eshop24/Program.cs
@@ -1,7 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using UTB.Eshop.Infrastructure.Database;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+string connectionString = builder.Configuration.GetConnectionString("MySQL");
+ServerVersion serverVersion = new MySqlServerVersion("8.0.38");
+builder.Services.AddDbContext<EshopDbContext>(optionsBuilder => optionsBuilder.UseMySql(connectionString, serverVersion));
 
 var app = builder.Build();
 
