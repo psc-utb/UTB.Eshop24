cc064d4e22d5a8ccbec011fc567c03558e51d273
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2024-09-18 15:34:15 +0200

    Oblasti (Areas) a routování
    + Přidání Area Admin (s vytvořením potřebných složek v projektu -> Areas, Areas/Admin, Areas/Admin/Controllers, Areas/Admin/Views, Areas/Admin/Views/Product)
    ~ Přidání routování na oblasti do Program.cs
    + Přidán ProductController v Area Admin s atributem propojujícím Controller a oblast
    + Přidána metoda Select do ProductController s návratem View
    + Přidáno View Select pro ProductController
    + Přidán odkaz na administraci produktů do Layoutu
    + Přidány/Zkopírovány soubory _ViewImports a _ViewStart do area Admin

---
+++ UTB.Eshop24/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.Eshop24.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Select()
+        {
+            return View();
+        }
+    }
+}
+
