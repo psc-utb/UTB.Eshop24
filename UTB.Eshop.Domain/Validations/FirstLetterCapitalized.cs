e4913de6bc4a10a1d318844ec3f746ef6b9cad8a
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-09-20 19:40:15 +0200

    Vytvoření vlastního validačního atributu (klientská část)
    + Přidání odkazu na Framework Microsoft.AspNetCore.App do konfiguračního souboru projektu vrstvy Domain -> je nutné pro použití interface IClientModelValidator
    ~ Přidání implementace interface IClientModelValidator do validačního atributu FirstLetterCapitalizedCZAttribute -> jedná se zde o přidání HTML atributů, které jsou využity klientskou validací
    + Přidán JavaScriptový soubor FirstLetterCapitalizedCZ.js s implementací klientské validace pro knihovnu jQuery Validation
    ~ Přidání odkazu na JavaScriptový soubor do Create View

--- UTB.Eshop.Domain/Validations/FirstLetterCapitalized.cs
+++ UTB.Eshop.Domain/Validations/FirstLetterCapitalized.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace UTB.Eshop.Domain.Validations
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
-    public class FirstLetterCapitalizedCZAttribute : ValidationAttribute
+    public class FirstLetterCapitalizedCZAttribute : ValidationAttribute, IClientModelValidator
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
@@ -33,6 +34,13 @@ namespace UTB.Eshop.Domain.Validations
             {
                 throw new NotImplementedException($"The {nameof(FirstLetterCapitalizedCZAttribute)} is not implemented for the type: {value.GetType()}");
             }
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (context.Attributes.ContainsKey("data-val") == false)
+                context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-firstlettercapcz", $"The {context.ModelMetadata.Name} field does not contain the first capital letter.");
         }
     }
 }
---
+++ UTB.Eshop24/wwwroot/js/validations/FirstLetterCapitalizedCZ.js
@@ -0,0 +1,27 @@
+﻿$.validator.addMethod('firstlettercapcz',
+function (value, element, params)
+{
+    if (!value || value == '')
+        return true;
+
+    if (Array.from(value)[0] >= 'A' && Array.from(value)[0] <= 'Z'
+      || Array.from(value)[0] == 'Š' || Array.from(value)[0] == 'Č' || Array.from(value)[0] == 'Ř' || Array.from(value)[0] == 'Ž'
+      || Array.from(value)[0] == 'Ý' || Array.from(value)[0] == 'Á' || Array.from(value)[0] == 'Í' || Array.from(value)[0] == 'É'
+      || Array.from(value)[0] == 'Ú' || Array.from(value)[0] == 'Ď' || Array.from(value)[0] == 'Ň' || Array.from(value)[0] == 'Ó'
+      || Array.from(value)[0] == 'Ť' || Array.from(value)[0] == 'Ě' || Array.from(value)[0] == 'Ů')
+    {
+        return true;
+    }
+
+    return false;
+});
+
+
+$.validator.unobtrusive.adapters.add('firstlettercapcz', '',
+function (options)
+{
+    var element = $(options.form).find('#Description')[0];
+
+    options.rules['firstlettercapcz'] = [element, ''];
+    options.messages['firstlettercapcz'] = options.message;
+});
