71deec29dc29dc57d9aa3c53ecb221a09dcffa60
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-09-20 19:35:27 +0200

    Vytvoření vlastního validačního atributu (serverová část)
    + Přidání vlastního validačního atributu pro kontrolu prvního velkého písmena v textu (stringu), včetně diakritiky a variant, které se běžně na začátku slov v češtině nevyskytují.
    ~ Aplikování vlastního validačního atributu na property Description v entitě Product

--- UTB.Eshop.Domain/Entities/Product.cs
+++ UTB.Eshop.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using UTB.Eshop.Domain.Validations;
 
 namespace UTB.Eshop.Domain.Entities
 {
@@ -9,6 +10,7 @@ namespace UTB.Eshop.Domain.Entities
         [Required]
         [StringLength(70)]
         public string? Name { get; set; }
+        [FirstLetterCapitalizedCZ]
         public string? Description { get; set; }
         public double Price { get; set; }
         public string? ImageSrc { get; set; }
---
+++ UTB.Eshop.Domain/Validations/FirstLetterCapitalized.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UTB.Eshop.Domain.Validations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class FirstLetterCapitalizedCZAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            else if (value is string text)
+            {
+                if (text == String.Empty)
+                    return ValidationResult.Success;
+
+                if (text.First() >= 'A' && text.First() <= 'Z'
+                 || text.First() == 'Š' || text.First() == 'Č' || text.First() == 'Ř' || text.First() == 'Ž'
+                 || text.First() == 'Ý' || text.First() == 'Á' || text.First() == 'Í' || text.First() == 'É'
+                 || text.First() == 'Ú' || text.First() == 'Ď' || text.First() == 'Ň' || text.First() == 'Ó'
+                 || text.First() == 'Ť' || text.First() == 'Ě' || text.First() == 'Ů')
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"The {validationContext.MemberName} field does not contain the first capital letter.");
+                }
+            }
+            else
+            {
+                throw new NotImplementedException($"The {nameof(FirstLetterCapitalizedCZAttribute)} is not implemented for the type: {value.GetType()}");
+            }
+        }
+    }
+}
