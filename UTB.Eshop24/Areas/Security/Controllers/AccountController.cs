ba701a71f5fc2effdbf3f310a6954c819268e554
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2024-10-06 16:48:00 +0200

    Registrační a přihlašovací formulář
    + Vytvoření area Security s Controllerem Account (zatím jen pro zobrazení formulářů)
    + Přidání CSS souboru (login.css) pro registrační a přihlašovací formulář
    + Přidání Views pro areu Security -> kompletní formuláře pro registraci a přihlášení + _ViewImports a _ViewStart
    + Přidání zahrnutí CSS souboru ikonek Bootstrapu do hlavičky v Layoutu
    + Přidání tlačítek pro přihlášení, registraci a odhlášení do navbaru v Layoutu

---
+++ UTB.Eshop24/Areas/Security/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.Eshop24.Areas.Security.Controllers
+{
+    [Area("Security")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
