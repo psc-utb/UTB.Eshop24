cc3e06e88d1c94bce4e7e2d77edb4431b7bee508
psc-utb <psc.utb@gmail.com>
2024-10-22 08:57:49 +0200

    Příprava pro upload souboru
    ~ Přidání property IFormFile Image do entity Product
         ¤ property není mapována do databáze, protože slouží jen jako místo, skrz které lze vložit soubor k uploadu
         ¤ pozn. pokud bychom neměli v projektu Domain.csproj referenci na Microsoft.AspNetCore.App, tak by nebylo možné IFormFile přidat
    ~změna formuláře v Create.cshtml, kde místo ImageSrc je přímo Image
         ¤ změněn i styl inputu, aby odpovídal uploadu souboru
    ~ změna enctype ve formuláři Create.cshtml na multipart/form-data
         ¤ bez této změny by nebylo možné odeslat soubor na server (v Image by byl null)

--- UTB.Eshop.Domain/Entities/Product.cs
+++ UTB.Eshop.Domain/Entities/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using UTB.Eshop.Domain.Validations;
+using Microsoft.AspNetCore.Http;
 
 namespace UTB.Eshop.Domain.Entities
 {
@@ -14,5 +15,8 @@ namespace UTB.Eshop.Domain.Entities
         public string? Description { get; set; }
         public double Price { get; set; }
         public string? ImageSrc { get; set; }
+
+        [NotMapped]
+        public IFormFile? Image { get; set; }
     }
 }
