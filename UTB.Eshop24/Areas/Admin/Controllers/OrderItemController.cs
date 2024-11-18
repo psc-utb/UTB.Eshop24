3155bc1ef1bb51d926f0577e19a943c6265c70bd
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-11-18 14:20:35 +0100

    Správa OrderItem (Controller a Views)
    + Přidání OrderItemControlleru pro správu OrderItems
         ¤ také ukázka SelectListu pro možnost vybírat jméno produktu a číslo objednávky místo jejich Id při vytváření OrderItem.
    + Přidány View Index a Create pro OrderItemController.
    + Přidán potřebný odkaz na akční metodu OrderItemControlleru do navbaru v _Layout.cshtml (jen po přihlášení Admina nebo Managera).

---
+++ UTB.Eshop24/Areas/Admin/Controllers/OrderItemController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using UTB.Eshop.Application.Abstraction;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Infrastructure.Identity.Enums;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UTB.Eshop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = nameof(Roles.Admin) + ", " + nameof(Roles.Manager))]
+    public class OrderItemController : Controller
+    {
+        IOrderItemAppService _orderItemService;
+        IProductAppService _productService;
+        IOrderAppService _orderService;
+        public OrderItemController(IOrderItemAppService orderItemService, IProductAppService productService, IOrderAppService orderService)
+        {
+            _orderItemService = orderItemService;
+            _productService = productService;
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            IList<OrderItem> orderItems = _orderItemService.Select();
+            return View(orderItems);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            SetOrderAndProductSelectLists();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _orderItemService.Create(orderItem);
+                return RedirectToAction(nameof(OrderItemController.Index));
+            }
+            else
+            {
+                SetOrderAndProductSelectLists();
+                return View(orderItem);
+            }
+        }
+
+        void SetOrderAndProductSelectLists()
+        {
+            IList<Product> products = _productService.Select();
+            ViewData[nameof(OrderItem.ProductID)] = new SelectList(products, nameof(Product.Id), nameof(Product.Name));
+
+            IList<Order> orders = _orderService.Select();
+            ViewData[nameof(OrderItem.OrderID)] = new SelectList(orders, nameof(Order.Id), nameof(Order.OrderNumber));
+        }
+    }
+}
+
