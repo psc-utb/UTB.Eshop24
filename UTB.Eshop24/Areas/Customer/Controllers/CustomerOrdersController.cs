a58cfe5723a38bde8efa213915f906bf42dd64ad
Tomas Vogeltanz <vogeltanz@utb.cz>
2024-11-18 14:46:03 +0100

    Zobrazení objednávek přihlášeného uživatele
    + Přidána Area Customer.
    + Přidán CustomerOrdersController pro obsloužení requestu pro zobrazení objednávek přihlášeného uživatele s rolí Customer.
    + Přidáno View pro CustomerOrdersController.
    + Přidán odkaz na akční metodu CustomerOrdersController do navbaru do nového dropdown menu v _Layout.cshtml.

---
+++ UTB.Eshop24/Areas/Customer/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UTB.Eshop.Application.Abstraction;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Infrastructure.Identity;
+using UTB.Eshop.Infrastructure.Identity.Enums;
+
+namespace UTB.Eshop.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles = nameof(Roles.Customer))]
+    public class CustomerOrdersController : Controller
+    {
+
+        ISecurityService _securityService;
+        IOrderAppService _orderService;
+
+        public CustomerOrdersController(ISecurityService securityService, IOrderAppService orderService)
+        {
+            _securityService = securityService;
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            User currentUser = await _securityService.GetCurrentUser(User);
+            if (currentUser != null)
+            {
+                IList<Order> userOrders = _orderService.SelectForUser(currentUser.Id);
+                return View(userOrders);
+            }
+
+            return NotFound();
+        }
+    }
+}
