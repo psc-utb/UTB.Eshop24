1ff35ca98bd28de51ccaf02cdd2f656654e72b47
Tomas Vogeltanz <vogeltanz@utb.cz>
2025-09-18 17:00:41 +0200

    JS code update for First Letter Capitalized Validation
    ~ JavaScript code update for "First Letter Capitalized" Validation. (it was added to synchronize JS code with C# code in the merge).

--- UTB.Eshop24/wwwroot/js/validations/FirstLetterCapitalizedCZ.js
+++ UTB.Eshop24/wwwroot/js/validations/FirstLetterCapitalizedCZ.js
@@ -4,11 +4,9 @@ function (value, element, params)
     if (!value || value == '')
         return true;
 
-    if (Array.from(value)[0] >= 'A' && Array.from(value)[0] <= 'Z'
-      || Array.from(value)[0] == 'Š' || Array.from(value)[0] == 'Č' || Array.from(value)[0] == 'Ř' || Array.from(value)[0] == 'Ž'
-      || Array.from(value)[0] == 'Ý' || Array.from(value)[0] == 'Á' || Array.from(value)[0] == 'Í' || Array.from(value)[0] == 'É'
-      || Array.from(value)[0] == 'Ú' || Array.from(value)[0] == 'Ď' || Array.from(value)[0] == 'Ň' || Array.from(value)[0] == 'Ó'
-      || Array.from(value)[0] == 'Ť' || Array.from(value)[0] == 'Ě' || Array.from(value)[0] == 'Ů')
+
+    const firstChar = value[0];
+    if (firstChar.toUpperCase() === firstChar && firstChar.toLowerCase() !== firstChar)
     {
         return true;
     }
